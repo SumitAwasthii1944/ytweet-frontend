35243ce125594f5968a2f4b8318d7196dae17a79
SumitAwasthii1944 <sumit.24660@knit.ac.in>
2026-06-06 02:17:56 +0530

    some changes

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_BASE = (import.meta.env.VITE_API_URL as string) || "http://localhost:8000"
+const API_BASE = (import.meta.env.VITE_API_URI as string) || "http://localhost:8000"
 const axiosInstance = axios.create({
     baseURL: `${API_BASE}/api/v1`,
     withCredentials: true,//sends cookies automatically
